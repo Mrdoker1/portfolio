ddc62041ed71eb881cda5040edcc5ad4a7272a9e
Viachas Kul <fixrapdoK@gmail.com>
2025-02-24 02:28:43 +0300

    chore: comment out unused linkButton example in element_examples.js

--- src/script/element_examples.js
+++ src/script/element_examples.js
@@ -1,11 +1,11 @@
-const linkButton = {
-    "type": "linkButton",
-    "name": "internalLinkButton",
-    "value": {
-        "text": "CONTACT ME",
-        "url": "?path=contact"
-    },
-    "propertes": {
-        "margin": "top-25px"
-    }
-}
+// const linkButton = {
+//     "type": "linkButton",
+//     "name": "internalLinkButton",
+//     "value": {
+//         "text": "CONTACT ME",
+//         "url": "?path=contact"
+//     },
+//     "propertes": {
+//         "margin": "top-25px"
+//     }
+// }
