932c9435ef5dabe06c2eb22087680c5424dd59f3
Viachas Kul <fixrapdoK@gmail.com>
2025-02-24 16:34:37 +0300

    fix: replace alert with console.log for error handling in PageBuilder

--- index.js
+++ index.js
@@ -3,6 +3,5 @@ import PageBuilder from "./src/script/script.js"
 try {
     new PageBuilder();
 } catch (e) {
-    alert(JSON.stringify(e.type));
+    console.log(JSON.stringify(e.type, e.message));
 }
-window.addEventListener("error", (e) => alert(JSON.stringify(e.message)));
